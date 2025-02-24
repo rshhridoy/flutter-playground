2baf17f6d7036f1f0f148f94560667547311a5cb
Hridoy <shakil15-4351@diu.edu.bd>
2025-02-24 06:24:58 +0600

    Animated Widget

--- lib/Expresso.dart
+++ lib/Expresso.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:wscubetech/StartingScreen.dart';
 
-class Expresso extends StatelessWidget {
+class Expresso extends StatefulWidget {
   var nameFromHome;
 
-  Expresso(this.nameFromHome);
+  Expresso(this.nameFromHome, {super.key});
+
+  @override
+  State<Expresso> createState() => _ExpressoState();
+}
+
+class _ExpressoState extends State<Expresso> {
+  var _decoration;
+
+  var _height;
+
+  var _width;
+
+  bool _change = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +34,38 @@ class Expresso extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            RichText(
-                text: TextSpan(
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.brown),
-                    children: [
-                  TextSpan(
-                      text: "Hello!!!, $nameFromHome ",
-                      style: TextStyle(fontSize: 25, color: Colors.teal[300])),
-                  TextSpan(text: "This is "),
-                  TextSpan(
-                      text: "Sabrina Carpenter",
-                      style: TextStyle(fontSize: 29, color: Colors.pinkAccent))
-                ])),
+            AnimatedContainer(
+              duration: Duration(seconds: 2),
+              curve: Curves.bounceOut,
+              decoration: _decoration,
+              height: _height,
+              width: _width,
+              child: RichText(
+                  text: TextSpan(
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.brown),
+                      children: [
+                    TextSpan(
+                        text: "Hello!!!, ",
+                        style:
+                            TextStyle(fontSize: 25, color: Colors.teal[300])),
+                    TextSpan(
+                        text: "${widget.nameFromHome}.  ",
+                        style: TextStyle(
+                            fontFamily: "Trajan Pro",
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold)),
+                    TextSpan(text: "This is "),
+                    TextSpan(
+                        text: "Sabrina Carpenter",
+                        style:
+                            TextStyle(fontSize: 29, color: Colors.pinkAccent))
+                  ])),
+            ),
             Padding(
               padding: const EdgeInsets.all(15.0),
               child: ElevatedButton(
@@ -56,7 +85,29 @@ class Expresso extends StatelessWidget {
                         fontSize: 20,
                         fontFamily: "Oswald"),
                   )),
-            )
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (true) {
+                      _height = 200.0;
+                      _width = 100.0;
+                      _decoration = BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.brown,
+                      );
+                      _change = false;
+                    } else {
+                      _height = 100.0;
+                      _width = 200.0;
+                      _decoration = BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.pinkAccent);
+                      _change = true;
+                    }
+                  });
+                },
+                child: Text("Change Resolution"))
           ],
         ),
       ),
